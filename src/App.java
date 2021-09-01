3da339e6421e30ce08a01b2684f7c7519f920008
Svvet <krzysztof.trepka@student.wat.edu.pl>
2021-09-02 01:02:13 +0200

    Creates directories in Dirs and Dirs directory if they don't exist

--- src/App.java
+++ src/App.java
@@ -17,7 +17,7 @@ public class App {
             attr = Files.readAttributes(fl.toPath(), BasicFileAttributes.class);
 
             if (type.equals("xml")) {
-                Path copied = Paths.get("Foldery/DEV/" + name);
+                Path copied = Paths.get("Dirs/DEV/" + name);
                 Path originalPath = fl.toPath();
                 Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
                 fl.delete();
@@ -28,13 +28,13 @@ public class App {
                 int hour = Integer.parseInt(attr.creationTime().toString().substring(12, 13));
                 
                 if (hour % 2 == 0) {
-                    Path copied = Paths.get("Foldery/DEV/" + name);
+                    Path copied = Paths.get("Dirs/DEV/" + name);
                     Path originalPath = fl.toPath();
                     Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
                     fl.delete();
                     return true;
                 } else {
-                    Path copied = Paths.get("Foldery/TEST/" + name);
+                    Path copied = Paths.get("Dirs/TEST/" + name);
                     Path originalPath = fl.toPath();
                     Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
                     fl.delete();
@@ -49,15 +49,20 @@ public class App {
     }
 
     public static void main(String[] args) {
+        new File("Dirs").mkdirs();
+        new File("Dirs/HOME").mkdirs();
+        new File("Dirs/DEV").mkdirs();
+        new File("Dirs/TEST").mkdirs();
+
         int counter = 0;
         try {
-            File myObj = new File("Foldery/HOME/count.txt");
+            File myObj = new File("Dirs/HOME/count.txt");
             if (myObj.createNewFile()) {
                 System.out.println("count.txt created: " + myObj.getName());
             } else {
                 System.out.println("count.txt already exists.");
             }
-            FileWriter myWriter = new FileWriter("Foldery/HOME/count.txt", false);
+            FileWriter myWriter = new FileWriter("Dirs/HOME/count.txt", false);
                     
                     myWriter.write(Integer.toString(counter));
                     myWriter.close();
@@ -70,7 +75,7 @@ public class App {
         try {
             WatchService watchService = FileSystems.getDefault().newWatchService();
 
-            Path path = Paths.get("Foldery/HOME");
+            Path path = Paths.get("Dirs/HOME");
             path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
                     StandardWatchEventKinds.ENTRY_MODIFY);
 
@@ -82,11 +87,11 @@ public class App {
                    
                     WatchEvent<Path> ev = cast(event);
                     Path filename = ev.context();
-                    File fll = new File("Foldery/HOME/" + filename);
+                    File fll = new File("Dirs/HOME/" + filename);
                     boolean success = toFolder(fll);
                     if (!success)
                         break;
-                    FileWriter myWriter = new FileWriter("Foldery/HOME/count.txt", false);
+                    FileWriter myWriter = new FileWriter("Dirs/HOME/count.txt", false);
                     counter++;
                     myWriter.write(Integer.toString(counter));
                     myWriter.close();
